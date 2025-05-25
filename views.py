df19da7f5a2b007688913c631b3f93e56659941c
Rennanpaulino <rennanpsousa2003@gmail.com>
2025-05-24 22:08:50 -0300

    Criadas as telas html para login, sign up e resultados, criado o arquivo css, sql, alterados arquivos view (adicionando as rotas para as novas telas) e README (corrigidos erros de texto)

--- views.py
+++ views.py
@@ -6,3 +6,15 @@ from flask import render_template
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/signin")
+def sign_in():
+    return render_template("login.html")
+
+@app.route("/signup")
+def sign_up():
+    return render_template("signup.html")
+
+@app.route("/results")
+def results():
+    return render_template("results.html")    
